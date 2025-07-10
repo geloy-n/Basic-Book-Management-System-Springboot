9175239308fde3199f1148d23deca9e8d4f7305c
Angelo <angelonieves0105@gmail.com>
2025-07-10 20:34:11 +0800

    attributes, constructors, getters/setters, and toString

--- src/main/java/com/bookmanagementsystem/entity/Book.java
+++ src/main/java/com/bookmanagementsystem/entity/Book.java
@@ -1,4 +1,76 @@
 package com.bookmanagementsystem.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name="genre")
+    private String genre;
+
+    // Constructors
+    public Book() {}
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    // getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
 }
