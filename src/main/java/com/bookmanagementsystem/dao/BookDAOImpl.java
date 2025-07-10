9d379111dc33a83b6d77065878be02b4d8ebcf4e
Angelo <angelonieves0105@gmail.com>
2025-07-10 20:32:28 +0800

    overide methods save, findById, and findAll

--- src/main/java/com/bookmanagementsystem/dao/BookDAOImpl.java
+++ src/main/java/com/bookmanagementsystem/dao/BookDAOImpl.java
@@ -1,4 +1,36 @@
 package com.bookmanagementsystem.dao;
 
-public class BookDAOImpl {
+import com.bookmanagementsystem.entity.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BookDAOImpl implements BookDAO {
+
+    private final EntityManager entityManager;
+
+    public BookDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Book book) {
+        entityManager.persist(book);
+    }
+
+    @Override
+    public Book findById(int id) {
+        return entityManager.find(Book.class, id);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        TypedQuery<Book> query = entityManager.createQuery("From Book", Book.class);
+        return query.getResultList();
+    }
 }
